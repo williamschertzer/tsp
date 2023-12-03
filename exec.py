354768f3c93ea931e022d6f04a719e77e6ecacc2
sonakshi11 <sonakshi1104@gmail.com>
2023-12-03 13:40:24 -0500

    adding exec file

---
+++ exec.py
@@ -0,0 +1,16 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-inst', type=str, required=True, help='Input file name')
+parser.add_argument('-alg', type=str, choices=['BF', 'Approx', 'LS'], required=True, help='Algorithm type: BF (Brute Force), Approx (Approximation), or LS (Local Search)')
+parser.add_argument('-time', type=int, required=True, help='Cutoff time in seconds')
+parser.add_argument('-seed', type=int, help='Random seed')
+
+
+args = parser.parse_args()
+
+print(f"Filename: {args.inst}")
+print(f"Algorithm: {args.alg}")
+print(f"Time cutoff: {args.time} seconds")
