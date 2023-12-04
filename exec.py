192e992c0319f4ebdb3eefb1d5870daebea79dc6
sonakshi11 <sonakshi1104@gmail.com>
2023-12-04 12:13:22 -0500

    changes to exec

--- exec.py
+++ exec.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-inst', type=str, required=True, help='Input file name')
 parser.add_argument('-alg', type=str, choices=['BF', 'Approx', 'LS'], required=True, help='Algorithm type: BF (Brute Force), Approx (Approximation), or LS (Local Search)')
-parser.add_argument('-time', type=int, required=True, help='Cutoff time in seconds')
+parser.add_argument('-time', type=int, help='Cutoff time in seconds')
 parser.add_argument('-seed', type=int, help='Random seed')
 
 
@@ -19,14 +19,18 @@ print(f"Algorithm: {args.alg}")
 print(f"Time cutoff: {args.time} seconds")
 
 if args.alg == 'BF':
-    visited, length = brute_force(args.inst, args.time)
+    best_tour, best_cost = brute_force(args.inst, args.time)
 
 elif args.alg == 'Approx':
-    quality, sol = approx(filename= args.inst)
+    best_cost, best_tour = approx(filename= args.inst)
 
 elif args.alg == 'LS':
     best_tour, best_cost = ls_run(args.inst, args.time, args.seed)
 
+
+with open(f'{args.inst}_{args.alg}_{args.time}_{args.seed}.sol', 'w') as file:
+    file.write(f'Length: {best_cost}')
+    file.write(f'Vertex_IDs: {best_tour}')
 #example: python exec.py -inst "Toronto.tsp" -alg "LS" -time 10
 
 
--- exec.sh
+++ exec.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
- 
+
+chmod +x exec.sh
 python exec.py
